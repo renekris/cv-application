0c6f61e7650e09ab973376191b7842570179c769
Renekris <renekrispohlak@gmail.com>
2022-12-21 18:24:40 +0200

    Update overview component to display all cv data

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import format from 'date-fns/format';
+import './Overview.css';
 
-const GeneralData = ({ general }) => {
-  const { dob, email, name, phone } = general;
+const UserHeader = ({ userData }) => {
+  const { dob, email, name, phone } = userData;
   return (
     <div>
       <p>Name: {name}</p>
@@ -12,10 +14,117 @@ const GeneralData = ({ general }) => {
   )
 }
 
+const Summary = ({ summaryData }) => {
+  return (
+    <div>
+      <h2>Summary</h2>
+      <p>{summaryData}</p>
+    </div>
+  )
+}
+
+const WorkExperience = ({ experienceData }) => {
+  return (
+    <div>
+      <h2>Work Experience</h2>
+      {experienceData.map((data, i) => {
+        return (
+          <div key={i}>
+            <p>{data.company}</p>
+            <p>{format(data.dateFrom, 'yyyy LLL')} - {format(data.dateTo, 'yyyy LLL')}</p>
+            <p>{data.position}</p>
+            <p>Tasks: <br />{data.mainTasks}</p>
+            <p>{data.additionalInfo}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const Education = ({ educationData }) => {
+  return (
+    <div>
+      <h2>Education</h2>
+      {educationData.map((data, i) => {
+        return (
+          <div key={i}>
+            <p>{data.school}</p>
+            <p>{format(data.dateFrom, 'yyyy LLL')} - {format(data.dateTo, 'yyyy LLL')}</p>
+            <p>{data.title}</p>
+            <p>{data.additionalInfo}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const LanguageSkills = ({ languageData }) => {
+  console.log(languageData[0])
+  return (
+    <div>
+      <h2>Language Skills</h2>
+      {languageData.map((data, i) => {
+        return (
+          <div key={i}>
+            <p>{data.language} - {data.skillLevel}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const ComputerSkill = ({ skillData, title, cName }) => {
+  return (
+    <div className={cName}>
+      <p>
+        {title}
+        {skillData.map((data, i, arr) => {
+          return (
+            <React.Fragment key={i}>{data}{i < arr.length - 1 ? ',' : ''} </React.Fragment>
+          )
+        })}
+      </p>
+    </div>
+  )
+}
+
+const ComputerSkills = ({ skillsData }) => {
+  console.log(skillsData)
+  return (
+    <div>
+      <h2>Computer Skills</h2>
+      <ComputerSkill skillData={skillsData.expert} title='Expert: ' cName='expert' />
+      <ComputerSkill skillData={skillsData.advanced} title='Advanced: ' cName='advanced' />
+      <ComputerSkill skillData={skillsData.intermediate} title='Intermediate: ' cName='intermediate' />
+      <ComputerSkill skillData={skillsData.average} title='Average user: ' cName='average' />
+      <ComputerSkill skillData={skillsData.beginner} title='Beginner: ' cName='beginner' />
+    </div>
+  )
+}
+
+const Other = ({ otherData }) => {
+  return (
+    <div>
+      <h2>Other</h2>
+      <p>{otherData.otherInfo}</p>
+    </div>
+  )
+}
+
 export const Overview = ({ cvData }) => {
   return (
     <div>
-      <GeneralData general={cvData.general} />
+      <h1>Your CV</h1>
+      <UserHeader userData={cvData.general} />
+      <Summary summaryData={cvData.summary} />
+      <WorkExperience experienceData={cvData.workExperience} />
+      <Education educationData={cvData.education} />
+      <LanguageSkills languageData={cvData.languageSkills} />
+      <ComputerSkills skillsData={cvData.computerSkills} />
+      <Other otherData={cvData.other} />
     </div>
   )
 }
