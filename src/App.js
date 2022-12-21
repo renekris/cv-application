83568f637470bec9618c6924e8bcb33bc53705b3
Renekris <renekrispohlak@gmail.com>
2022-12-21 16:13:59 +0200

    Update CV with better structured data

--- src/App.js
+++ src/App.js
@@ -7,25 +7,44 @@ export default class CVApp extends Component {
 
     this.state = {
       cv: {
+        summary: 'Currently I am learning Full Stack via The Odin Project. \nCompleted projects: https://github.com/renekris',
         general: {
           name: 'Renekris',
           email: 'renekrispohlak@gmail.com',
           phone: '5555 5555',
           dob: new Date(1007433661000),
         },
-        experience: {
-          school: {
-            name: 'TTHK',
+        education: {
+          'TTHK': {
             title: 'Software Development',
-            date: new Date(1625346000000),
-          },
-          work: {
-            company: 'SMIT',
+            dateFrom: new Date(1625346000000),
+            dateTo: new Date(1626346000000),
+            additionalInfo: 'EQF level 4 qualification as a junior IT systems specialist.',
+          }
+        },
+        workExperience: {
+          'SMIT': {
             position: 'IT Technician',
-            mainTasks: 'Fix hardware',
             dateFrom: new Date(1581026400000),
             dateTo: new Date(1596747600000),
+            mainTasks: 'Fix hardware',
+            additionalInfo: 'Responded to tickets and fixed internal problems',
           }
+        },
+        languageSkills: {
+          estonian: 'Native',
+          english: 'C2',
+          japanese: 'B2',
+        },
+        computerSkills: {
+          expert: [],
+          advanced: ['Javascript', 'C#', 'Linux', 'Windows'],
+          intermediate: ['SQL', 'Node.js', 'MySQL', 'HTML', 'CSS', 'Jira'],
+          average: ['React', 'Apache'],
+          beginner: ['Angular', 'Python'],
+        },
+        other: {
+          otherInfo: 'I love learning new and interesting computer-related stuff. I go out of my way to do testing with software and hardware to confirm or disprove my hypotheses.',
         }
       }
     };
